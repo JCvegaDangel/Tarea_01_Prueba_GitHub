ff98971f7ed4ba43d5c90606cb595f2fc751d708
JCvegaDangel <79423707+JCvegaDangel@users.noreply.github.com>
2021-02-26 21:34:25 -0600

    Add files via upload
    Primer tarea del curso de Programación Orientada a Objetos

---
+++ Hola.cpp
@@ -0,0 +1,15 @@
+// VEGA DEL ANGEL JUAN CARLOS 2AV6
+// PROGRAMA 1, Hola <Tu nombre>
+
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+int main()
+{
+	string n;
+	cout << "Dime tu nombre: "<<endl;	
+	cin >> n;
+	cout<< "Hola "<<n<<endl;
+}
